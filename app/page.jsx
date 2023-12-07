3204087ba64da4e43d805260cb969024291ffa25
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-07 18:47:09 +0300

    add reviews page

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam natus repellendus soluta, dolores dignissimos ea! Ut quas repudiandae officiis recusandae.</p>
+    </>
+  )
+}
---
+++ app/reviews/page.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function page() {
+  return (
+    <>
+      <h1>Home Page</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam natus
+        repellendus soluta, dolores dignissimos ea! Ut quas repudiandae officiis
+        recusandae.
+      </p>
+    </>
+  );
+}
+
+export default page
