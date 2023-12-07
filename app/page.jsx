e1c5bbcbe7dce0c52a489c4378c448606b8ed15e
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-08 01:46:42 +0300

    edit main page

--- app/page.jsx
+++ app/page.jsx
@@ -3,8 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <>
-      <h1>Home Page</h1>
-      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam natus repellendus soluta, dolores dignissimos ea! Ut quas repudiandae officiis recusandae.</p>
+      <h1>Hello</h1>
     </>
   )
 }
