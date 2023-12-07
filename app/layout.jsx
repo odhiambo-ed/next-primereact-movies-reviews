dc5b910703f76f87febc3cee7523cf54c845b4cc
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-08 02:27:49 +0300

    add saga primereact theme

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import 'primereact/resources/themes/saga-blue/theme.css';
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
 import Header from './components/Header';
