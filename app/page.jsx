cecbdc16e89af5dffe48cdf5213a1948c9720d86
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-11 18:42:12 +0300

    add trending to page

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Hero from './components/Hero'
+import Trending from './components/Trending'
 
 export default function Home() {
   return (
     <>
       <Hero></Hero>
+      <Trending></Trending>
     </>
   )
 }
