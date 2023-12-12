6e7627d8db1180f9301f6dc2a65f9fb8c04556cf
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-12 15:17:29 +0300

    add link to movie card

--- app/components/Trending.jsx
+++ app/components/Trending.jsx
@@ -1,20 +1,26 @@
 import { getTrendingMovies } from '@/utils/responses'
+import Link from 'next/link'
+
 async function Trending() {
     const movies = await getTrendingMovies();
   return (
-      <div>
-            <h2>Trending Movies</h2>
-            <ul>
-                {movies.map(movie => (
-                    <li key={movie.id}>
-                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-                        <h3>{movie.title}</h3>
-                        <p>{movie.overview}</p>
-                    </li>
-                ))}
-            </ul>
+    <div>
+      <h2>Trending Movies</h2>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link href={"/movies/" + movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              />
+              <h3>{movie.title}</h3>
+              <p>{movie.overview}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
 export default Trending
