cf3c326a33f7df5c652448b296cdd32289867ff2
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-21 20:07:59 +0300

    add rating range to card

--- app/components/Trending.jsx
+++ app/components/Trending.jsx
@@ -18,10 +18,33 @@ async function Trending() {
           <Card
             key={movie.id}
             header={
+              // <>
+              //   <div className="relative">
+              //     <div className="absolute top-0 right-0 z-10 bg-transparent rounded-full border-3 border-white p-3 m-1 text-white font-bold">
+              //       {movie.vote_average.toFixed(1)}
+              //     </div>
+              //     <img
+              //       src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              //       alt={movie.title}
+              //     />
+              //   </div>
+              // </>
               <>
                 <div className="relative">
-                  <div className="absolute top-0 right-0 z-10 bg-transparent rounded-full border-3 border-white p-3 m-1 text-white font-bold">
-                    {movie.vote_average.toFixed(1)}
+                  <div className="absolute top-0 right-0 z-10">
+                    <div
+                      className="relative rounded-full p-3 m-1 text-gray-900 font-bold overflow-hidden"
+                      style={{
+                        background: `linear-gradient(90deg, #F5F5F590 ${
+                          (movie.vote_average / 10) * 100
+                        }%, #ffffff00 ${(movie.vote_average / 10) * 100}%)`,
+                      }}
+                    >
+                      <div className="absolute inset-0 border-3 border-gray-400 rounded-full"></div>
+                      <div className="relative z-10">
+                        {movie.vote_average.toFixed(1)}
+                      </div>
+                    </div>
                   </div>
                   <img
                     src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
