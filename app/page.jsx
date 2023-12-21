db56158c1e1603f43f913df52f5026a07a248d13
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-21 17:00:01 +0300

    edit page

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import Hero from './components/Hero'
-import Trending from './components/Trending'
+import Hero from "./components/Hero";
+import Trending from "./components/Trending";
 
 export default function Home() {
   return (
-    <>
-      <Hero></Hero>
-      <Trending></Trending>
-    </>
-  )
+    <div className="flex flex-col">
+      <Hero />
+      <Trending />
+    </div>
+  );
 }
