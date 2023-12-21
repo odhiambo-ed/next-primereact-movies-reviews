9f681e5570ca1a5bfc2d3ecb1a67c29cd1cc1cf3
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-21 17:07:42 +0300

    add elements to card

--- app/components/Trending.jsx
+++ app/components/Trending.jsx
@@ -6,31 +6,49 @@ async function Trending() {
   const movies = await getTrendingMovies();
 
   return (
-    <div className="flex flex-wrap gap-4 py-10">
+    <div className="flex flex-wrap">
       {movies.map((movie) => (
         <Card
           key={movie.id}
           header={
-            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+            <img
+              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+              alt={movie.title}
+            />
           }
           footer={
             <>
+              <div>
+                <Rating
+                  value={movie.vote_average / 2}
+                  stars={5}
+                  readonly
+                  cancel={false}
+                />
+              </div>
               <div>{movie.release_date}</div>
-              <Rating
-                value={movie.vote_average / 2}
-                stars={5}
-                readonly
-                cancel={false}
-              />
             </>
           }
-          className="p-m-2 p-md-3 p-lg-4" // Adjust the margin and padding as needed
-          style={{ width: "calc(20% - 32px)" }} // Specify the width for 5 cards in a row
+          className="p-m-2 p-md-3 p-lg-4 movie-card"
+          style={{ width: "calc(20% - 32px)" }}
         >
           <h3>{movie.title}</h3>
-          <p style={{ maxHeight: "100px", overflow: "hidden" }}>
+          <div className="movie-genres">
+            {movie.genres.map((genre) => (
+              <span key={genre.id} className="p-tag p-mr-1">
+                {genre.name}
+              </span>
+            ))}
+          </div>
+          <p
+            className="movie-overview"
+            style={{ maxHeight: "100px", overflow: "hidden" }}
+          >
             {movie.overview}
           </p>
+          <a href={`/movie/${movie.id}`} className="p-button-link">
+            Read More
+          </a>
         </Card>
       ))}
     </div>
