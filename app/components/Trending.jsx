dfe803117d2b2b8076549b87c3f2180a779bf464
odhiambo-ed <odhiambo.edward111@gmail.com>
2023-12-21 18:53:07 +0300

    add link to movie card

--- app/components/Trending.jsx
+++ app/components/Trending.jsx
@@ -1,4 +1,5 @@
 import { getTrendingMovies } from "@/utils/responses";
+import Link from "next/link";
 import { Card } from "primereact/card";
 import { Rating } from "primereact/rating";
 
@@ -8,38 +9,43 @@ async function Trending() {
   return (
     <div className="flex flex-wrap gap-4 py-10">
       {movies.map((movie) => (
-        <Card
-          key={movie.id}
-          header={
-            <img
-              src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-              alt={movie.title}
-            />
-          }
-          footer={
-            <>
-              <div>
-                <Rating
-                  value={movie.vote_average / 2}
-                  stars={5}
-                  readonly
-                  cancel={false}
-                />
-              </div>
-              <div>{movie.release_date}</div>
-            </>
-          }
+        <Link 
+          href={`/movie/${movie.id}`} 
+          key={movie.id} 
           className="p-m-2 p-md-3 p-lg-4" // Adjust the margin and padding as needed
           style={{ width: "calc(20% - 32px)" }} // Specify the width for 5 cards in a row
         >
-          <h3>{movie.title}</h3>
-          <p style={{ maxHeight: "100px", overflow: "hidden" }}>
-            {movie.overview}
-          </p>
-          <a href={`/movie/${movie.id}`} className="p-button-link">
-            Read More
-          </a>
-        </Card>
+          <Card
+            key={movie.id}
+            header={
+              <img
+                src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                alt={movie.title}
+              />
+            }
+            footer={
+              <>
+                <div>
+                  <Rating
+                    value={movie.vote_average / 2}
+                    stars={5}
+                    readonly
+                    cancel={false}
+                  />
+                </div>
+                <div>{movie.release_date}</div>
+              </>
+            }
+          >
+            <h3>{movie.title}</h3>
+            <p style={{ maxHeight: "100px", overflow: "hidden" }}>
+              {movie.overview}
+            </p>
+            <a href={`/movie/${movie.id}`} className="p-button-link">
+              Read More
+            </a>
+          </Card>
+        </Link>
       ))}
     </div>
   );
